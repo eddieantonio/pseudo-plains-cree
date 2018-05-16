0cc31e0ed270400ad8b3669d7d46e5991e43f292
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-16 15:34:50 -0600

    Use property-testing.

--- test_generate.py
+++ test_generate.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+from hypothesis import given
+from hypothesis.strategies import builds
+
 from generate import generate
 
 VOWELS = 'aioâîôê'
 
 
-def test_basic_usage():
+@given(builds(generate))
+def test_basic_usage(utterance):
     """
     A produced utterance has at least one vowel in it.
     """
-    utterance = generate()
     assert any(v in utterance for v in VOWELS)
