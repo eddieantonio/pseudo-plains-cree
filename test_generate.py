2c589b45e0687904f16c03faa650b0f4951da25c
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-16 15:41:24 -0600

    Test generate consecutive vowels.

--- test_generate.py
+++ test_generate.py
@@ -14,4 +14,20 @@ def test_basic_usage(utterance):
     """
     A produced utterance has at least one vowel in it.
     """
-    assert any(v in utterance for v in VOWELS)
+    assert any(is_vowel(c) for c in utterance)
+
+
+@given(builds(generate))
+def test_vowels(utterance):
+    assert not any(is_vowel(g1) and is_vowel(g2)
+                   for g1, g2 in bigrams(utterance))
+
+
+def is_vowel(char: str) -> bool:
+    if len(char) != 1:
+        raise ValueError("expecting exactly one character; got " + repr(char))
+    return char in VOWELS
+
+
+def bigrams(text: str):
+    return zip(text, text[1:])
