7b11ea94a4a5b4fe10f732242833c431c334962d
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-16 16:04:41 -0600

    That's one way of doing it...

--- test_generate.py
+++ test_generate.py
@@ -2,25 +2,33 @@
 # -*- coding: UTF-8 -*-
 
 from hypothesis import given
-from hypothesis.strategies import builds
+from hypothesis.strategies import random_module
 
 from generate import generate
 
 VOWELS = 'aioâîôê'
 
 
-@given(builds(generate))
-def test_basic_usage(utterance):
+@given(random_module())
+def test_basic_usage(random):
     """
     A produced utterance has at least one vowel in it.
     """
+    utterance = generate()
     assert any(is_vowel(c) for c in utterance)
 
 
-@given(builds(generate))
-def test_vowels(utterance):
+@given(random_module())
+def test_vowels(random):
+    utterance = generate()
     assert not any(is_vowel(g1) and is_vowel(g2)
                    for g1, g2 in bigrams(utterance))
+
+
+@given(random_module())
+def test_doubled_grams(random):
+    utterance = generate()
+    assert all(g1 != g2 for g1, g2 in bigrams(utterance))
 
 
 def is_vowel(char: str) -> bool:
