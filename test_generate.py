d2cc2dc0063e9c8c7c69c06a895ca4bb822efad3
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-16 18:12:15 -0600

    Clean up test file.

--- test_generate.py
+++ test_generate.py
@@ -21,8 +21,6 @@ from hypothesis.strategies import random_module, composite  # type: ignore
 
 from generate import generate
 
-VOWELS = 'aioâîôê'
-
 
 @composite
 def utterances(draw):
@@ -40,20 +38,34 @@ def test_basic_usage(utterance):
 
 @given(utterances())
 def test_vowels(utterance):
+    """
+    It should not create sequences of two or more vowels side-by-side.
+    """
     assert not any(is_vowel(g1) and is_vowel(g2)
                    for g1, g2 in bigrams(utterance))
 
 
 @given(utterances())
 def test_doubled_grams(utterance):
+    """
+    It should not generate sequences where any letters are doubled.
+    """
     assert all(g1 != g2 for g1, g2 in bigrams(utterance))
 
 
+# ############################### Utilities ############################### #
+
 def is_vowel(char: str) -> bool:
+    """
+    Returns true if the character is a Plains Cree vowel.
+    """
     if len(char) != 1:
         raise ValueError("expecting exactly one character; got " + repr(char))
-    return char in VOWELS
+    return char in 'aioâîôê'
 
 
 def bigrams(text: str):
+    """
+    Yeilds each pair of characters in the string.
+    """
     return zip(text, text[1:])
